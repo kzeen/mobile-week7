52ce9a893fb4f8fce7bce06253b1259a57260e62
zeen <157370421+kzeen@users.noreply.github.com>
2025-03-11 16:13:35 +0200

    Make splash screen exit after 2 seconds

--- app/src/main/java/com/example/mobileweek7/SplashActivity.java
+++ app/src/main/java/com/example/mobileweek7/SplashActivity.java
@@ -1,6 +1,9 @@
 package com.example.mobileweek7;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,12 @@ public class SplashActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            // your code here to start next activity and finish current
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish(); // To not return to splash screen - destroys
+        }, 2000);
     }
 }
