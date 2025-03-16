cf6781f51ccd8d2a5b79b3e5c7186ad82b7772b3
zeeny <157370421+kzeen@users.noreply.github.com>
2025-03-16 23:50:17 +0200

    New initial commit

---
+++ app/src/main/java/com/example/mobileweek7/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.mobileweek7;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            // your code here to start next activity and finish current
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish(); // To not return to splash screen - destroys
+        }, 2000);
+    }
+}
